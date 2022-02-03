571349bc87b99283fa1e5a086d056a0c919f7cd0
Nate Gotz <nate.gotz@iparchitechs.com>
2022-02-03 08:27:15 -0600

    Initial push

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # clab-pdns
-Create records in PowerDNS-Admin from Containerlab nodes
+
+The clab-pdns.sh script will create DNS records for containerlab nodes. It can do this by either manually executing the script or setting up incron to watch the /etc/hosts file for updates
+
+This assumes that you are running [PowerDNS-Admin](https://github.com/PowerDNS-Admin/PowerDNS-Admin).
+
+## Setup
+
+This should be setup on the containerlab server
+
+### Updates needed
+
+Right now, all the variables are hardcoded into the script. You'll need to update the SERVER, APIKEY, DOMAIN, and ADD_IPV6 variables to match your environment. The server and APIKEY should match what is in PowerDNS-Admin.
+
+### Manual Running
+
+```bash
+sudo clab-pdns.sh
+```
+
+### Automatic DNS record updates
+
+This version requires that incron is installed on the containerlab server.
+
+To install on Debian based systems use:
+
+```bash
+sudo apt install incron
+```
+
+Then you need to allow users to run incron
+
+```bash
+echo 'root' | sudo tee -a /etc/incron.allow
+```
+
+Then you need to setup the incron to watch the /etc/hosts file
+
+```bash
+sudo incrontab -e
+```
+
+Add the following to the file and save. Make sure to put in the full path to the file
+
+```bash
+/etc/hosts      IN_MODIFY       <path>/clab-pdns.sh
+```
---
+++ clab-pdns.sh
@@ -0,0 +1,163 @@
+#!/bin/bash
+
+SERVER=''
+APIKEY=''
+DOMAIN=''
+ADD_IPV6=false
+
+HOSTNAME=$(hostname)
+
+# Verify that needed tools are on the computer (jq, curl, containerlab)
+if ! [ -x "$(command -v jq)" ] || ! [ -x "$(command -v curl)" ] || ! [ -x "$(command -v containerlab)" ]; then
+  echo "Need to install some packages"
+  # First figure out if it's YUM or DEB
+  UPDATER=""
+  if [ -f /etc/redhat-release ]; then
+    UPDATER="yum install"
+  else
+    UPDATER="apt install"
+  fi
+  # Verify that JQ is installed
+  if ! [ -x "$(command -v jq)" ]; then
+    echo "Installing jq"
+    sudo $UPDATER jq
+  fi
+
+  # Verify that curl is installed
+  if ! [ -x "$(command -v curl)" ]; then
+    echo "Installing curl"
+    sudo $UPDATER curl
+  fi
+
+  # Verify that containerlab is installed
+  if ! [ -x "$(command -v containerlab)" ]; then
+    echo "Installing containerlab"
+    sudo bash -c "$(curl -sL https://get-clab.srlinux.dev)"
+  fi
+fi
+
+# Get all of the running devices in containerlab
+ALL_CLABS=$(containerlab inspect --all -f json)
+
+# Get a list of just the container IDs that will be used to check if a record currently exists
+CONTAINER_IDS=$(jq ".[] | .container_id" <<< $ALL_CLABS)
+
+# Empty - will get filled in with existing records that match
+DNS_CONTAINER_IDS=""
+
+# Drop all lab A/AAAA records
+EXISTING_RECORDS=`curl -H 'Content-Type: application/json' -s -H "X-API-Key: ${APIKEY}" http://${SERVER}/api/v1/servers/localhost/zones/${DOMAIN} | jq -r ".rrsets[] | select(.type == (\"A\",\"AAAA\"))"`
+
+# Only delete the records if there are any existing A/AAAA records
+if [ -n "$EXISTING_RECORDS" ]; then
+  DELETE_RECORDS='{"rrsets":[]}'
+  while read i; do
+    read -ra COMMENT <<< "$(jq -r ".comments[0].content" <<< $i)"
+
+    if [ "$COMMENT" != "null" ]; then
+
+
+      # Check if the hostname matches the comment and if the container ID doesn't exist anymore
+      if [ ${COMMENT[0]} == "$HOSTNAME" ] && [[ ! $CONTAINER_IDS =~ "\"${COMMENT[1]}\"" ]]; then
+        
+        # do stuff with $i
+        RECORD=`echo "$i" | jq '.name'`
+        RECORD_TYPE=`echo "$i" | jq '.type'`
+        DELETE_RECORDS=`echo "$DELETE_RECORDS" |jq -r ".rrsets += [{\"name\": $RECORD, \"type\": $RECORD_TYPE, \"changetype\": \"DELETE\"}]"`
+
+      else
+        # If the hostname and container ID match, add it to this list, this will help prevent updating/recreating a new DNS record later
+        DNS_CONTAINER_IDS="\"${COMMENT[1]}\"\n${DNS_CONTAINER_IDS}"
+      fi
+    fi
+  done <<< "$(jq -c '.' <<< $EXISTING_RECORDS)"
+  
+  # Delete the removed A/AAAA records if there are updates
+  if [ "$DELETE_RECORDS" != "{\"rrsets\":[]}" ]; then
+    curl -s -H 'Content-Type: application/json' -X PATCH --data "${DELETE_RECORDS}" -H "X-API-Key: ${APIKEY}" http://${SERVER}/api/v1/servers/localhost/zones/${DOMAIN} | jq
+  fi
+fi
+
+
+NEW_RECORDS='{"rrsets":[]}'
+
+input="/etc/hosts"
+START_STRING="-START ######"
+END_STRING="-END ######"
+CLAB_NODES=false
+CHANGES=false
+
+# Read the /etc/hosts file
+while IFS= read -r line
+do
+  # Start of CLAB Nodes list
+  if [[ "$line" == *"$START_STRING" ]]; then
+    CLAB_NODES=true
+    LAB_NAME=$(grep -oP '(?<=CLAB-).*(?=-START)' <<< $line | awk '{print tolower($0)}')
+  # END OF CLAB_NODES list
+  elif [[ "$line" == *"$END_STRING" ]]; then
+    CLAB_NODES=false
+  else
+    if [ "$CLAB_NODES" = true ]; then
+      read -a strarr <<< "$line"
+      IP_ADDR=${strarr[0]}
+      
+      # Get the container ID
+      CONTAINER_ID=`jq -r ".[] | select(.name == \"${strarr[1]}\") | .container_id" <<< $ALL_CLABS`
+
+      COMMENT="$HOSTNAME $CONTAINER_ID"
+
+      # CLAB node name to DNS record name = 
+      # clab-<lab>-<name> to <name>.<lab>.<domain>
+
+      # Strip out clab- from the node name (if it exists)
+      NODE=$(sed 's/clab-//' <<< ${strarr[1]})
+
+      # If the lab name is in the node name, we want that at the end for DNS
+      if [[ "$NODE" == "$LAB_NAME-"* ]]; then
+        NODE=$(grep -oP "(?<=$LAB_NAME-).*" <<< ${NODE})
+        NODE="$NODE.$LAB_NAME"
+      else
+        NODE=$(grep -oP "(?<=$LAB_NAME-).*" <<< ${NODE})
+      fi
+
+      NODE="$NODE.$DOMAIN."
+
+      if [[ ! $DNS_CONTAINER_IDS =~ "\"${CONTAINER_ID}\"" ]]; then
+        # Have it setup to create AAAA records, but I'm not currently routing them in the home network since I'm a loser so, we skip those records
+        if [[ ${IP_ADDR} == *"."* || "$ADD_IPV6" = true  ]]; then
+          if [[ ${IP_ADDR} == *":"* ]]; then
+            TYPE="AAAA"
+          else
+            TYPE="A"
+          fi
+          NEW_RECORDS=`echo "$NEW_RECORDS" |jq ".rrsets += 
+            [
+              {
+                \"name\": \"$NODE\",
+                \"type\": \"$TYPE\",
+                \"ttl\": \"60\",
+                \"changetype\": \"REPLACE\",
+                \"records\": [
+                  {
+                    \"content\": \"${IP_ADDR}\",
+                    \"disabled\": false
+                  }
+                ],
+                \"comments\": [
+                  {
+                    \"account\": \"\",
+                    \"content\": \"${COMMENT}\"
+                  }
+                ]
+              }
+            ]"`
+        fi
+      fi
+    fi
+  fi
+done < "$input"
+
+if [ "$NEW_RECORDS" != "{\"rrsets\":[]}" ]; then
+  curl -s -H 'Content-Type: application/json' -X PATCH --data "${NEW_RECORDS}" -H "X-API-Key: ${APIKEY}" http://${SERVER}/api/v1/servers/localhost/zones/${DOMAIN} | jq .
+fi
